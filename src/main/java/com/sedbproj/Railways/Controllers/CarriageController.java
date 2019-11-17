ced09b20251f94bf592cef9c08bb67561853d2bf
almus <Almas>
2019-11-17 20:28:57 +0600

    createCarriage method in controller

--- src/main/java/com/sedbproj/Railways/Controllers/CarriageController.java
+++ src/main/java/com/sedbproj/Railways/Controllers/CarriageController.java
@@ -3,6 +3,7 @@ package com.sedbproj.Railways.Controllers;
 import com.sedbproj.Railways.Entities.CarriageEntity;
 import com.sedbproj.Railways.Repositories.CarriageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,11 @@ public class CarriageController {
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public List<CarriageEntity> getCarriagesByTrainId(@PathVariable("id") Integer id){
         return carriageRepository.getCarriageEntitiesByTrainId(id);
+    }
+
+    @CrossOrigin(origins="*")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createCarriage(Iterable<CarriageEntity> carriageEntity){
+        carriageRepository.saveAll(carriageEntity);
     }
 }
