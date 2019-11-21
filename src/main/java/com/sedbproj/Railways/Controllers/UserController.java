b30231c1ce9e04aa96f7a8337b9c72883e815e3c
almus <Almas>
2019-11-22 05:05:16 +0600

    WebSecured

--- src/main/java/com/sedbproj/Railways/Controllers/UserController.java
+++ src/main/java/com/sedbproj/Railways/Controllers/UserController.java
@@ -1,11 +1,26 @@
 package com.sedbproj.Railways.Controllers;
 
 
+import com.sedbproj.Railways.Entities.StationEntity;
+import com.sedbproj.Railways.Entities.UserEntity;
+import com.sedbproj.Railways.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
 
+    @CrossOrigin(origins="*")
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<UserEntity> getAllUsers(){
+        return userRepository.findAll();
+    }
 }
